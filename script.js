13f9d1431d86aff5234ceacc50b5b677f980c8b8
Ashton Beaudoin <ashtonbeaudoin2000@hotmail.com>
2019-12-18 23:44:20 -0600

    final changes

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function password (){
+    var characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-=[]',./_+{}|";
+    var randomPassword = "";
+
+    for var i = 0; i++){
+        randomPassword = Math.floor(Math.random() * characters.textBox);
+    }
+    document.getElementById("textBox") = randomPasswordl
+    
+}
